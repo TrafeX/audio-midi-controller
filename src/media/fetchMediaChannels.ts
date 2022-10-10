df2a2d0c0a51a99751dc6df103caafab94b7d650
Tim de Pater <git@trafex.nl>
2022-10-10 14:40:54 +0200

    Take into account hardware devices like HDMI

--- src/media/fetchMediaChannels.ts
+++ src/media/fetchMediaChannels.ts
@@ -2,7 +2,7 @@ import { exec } from '../helpers/exec';
 import { mediaChannelsType, mediaTypeType } from '../types';
 
 const sinkInputsRegex = /index: ([0-9]+).*?state: ([a-z]+).*?volume:.*?([0-9]+)%.*?muted: (yes|no).*?application\.name = "([a-z_ -]+)"/gis;
-const sinksRegex = /\* index: ([0-9]+).*?state: ([a-z]+).*?volume:.*?([0-9]+)%.*?muted: (yes|no).*?device\.description = "([a-z_ -]+)"/gis;
+const sinksRegex = /\* index: ([0-9]+).*?state: ([a-z]+).*?volume:.*?([0-9]+)%.*?muted: (yes|no).*?(?:device\.description|device\.product\.name) = "([a-z_ -]+)"/gis;
 const inputSourcesRegex = /\* index: ([0-9]+).*?state: ([a-z]+).*?volume:.*?([0-9]+)%.*?muted: (yes|no).*?device\.description = "([a-z_ -]+)"/gis;
 
 export const fetchMediaChannels = async (): Promise<mediaChannelsType> => {
